c759947c654171c0d49b62e106ec95389e0bd932
Craig Ford <craig_ford@icloud.com>
2022-11-18 18:07:56 -0500

    commit tree d0ae6467de4908b2fcc0bafaeda477f40efbf3f6

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,42 @@
-# your code here
+class Stack   
+
+    def initialize(value = 0)
+        @stack = []
+        if value.integer?
+            @limit = value
+        end
+    end
+
+    def push(value)
+        @stack.push(value)    
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek 
+        @stack.last
+    end
+
+    # def size
+    #     size = @stack.length
+    # end
+
+    # def empty? 
+    #     if @stack.length == 0 
+    #         return true
+    #     else
+    #         return false    
+    #     end
+    # end
+
+    # def full 
+    #     if @stack.length == @limit
+    #         true
+    #     else
+    #         false
+    #     end    
+    # end
+end
+
